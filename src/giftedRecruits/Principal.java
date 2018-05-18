fb28df43ea927316da40bafa58c82707ced71353
Pra3t0r5 <fernando.albertengo@gmail.com>
2018-05-18 18:10:12 -0300

    Prueba de commit n2

--- src/giftedRecruits/Principal.java
+++ src/giftedRecruits/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gifted Recruits training grounds!");
-
+		System.out.println("Requesting PUSH");
 	}
 
 }
